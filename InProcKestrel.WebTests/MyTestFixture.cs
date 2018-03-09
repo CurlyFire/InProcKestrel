328fc8eac500d80e1fcb45d3d56626e8f7bf7a2a
CurlyFire <stephane.denomme@inspq.qc.ca>
2018-03-09 09:55:57 -0500

    Fixed test csproj for libuv.dll and the missing dependencies

--- InProcKestrel.WebTests/MyTestFixture.cs
+++ InProcKestrel.WebTests/MyTestFixture.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using NUnit.Framework;
@@ -7,34 +6,35 @@ using OpenQA.Selenium.Chrome;
 
 namespace InProcKestrel.WebTests
 {
-  [TestFixture]
-  public class MyTestFixture
-  {
-    private IWebHost _host;
-    private string _baseAddress;
-
-    [SetUp]
-    public void SetUp ()
+    [TestFixture]
+    public class MyTestFixture
     {
-      _host = new WebHostBuilder()
-          .UseKestrel()
-          .UseContentRoot (@"C:\Users\myuser\Documents\visual studio 2017\Projects\InProcKestrel\InProcKestrel\")
-          .UseStartup<Startup>()
-          .Build();
+        private IWebHost _host;
+        private string _baseAddress;
 
-      _host.Start();
-      _baseAddress = _host.ServerFeatures.Get<IServerAddressesFeature>().Addresses.Single();
-    }
+        [SetUp]
+        public void SetUp()
+        {
+            _host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(@"C:\Git\InProcKestrel\InProcKestrel\")
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
 
-    [Test]
-    public void MyTest ()
-    {
-      using (var webDriver = new ChromeDriver())
-      {
-        webDriver.Url = _baseAddress;
-        var body = webDriver.FindElementByTagName ("body").Text;
-        Assert.That (body, Is.Not.Empty);
-      }
+            _host.Start();
+            _baseAddress = _host.ServerFeatures.Get<IServerAddressesFeature>().Addresses.Single();
+        }
+
+        [Test]
+        public void MyTest()
+        {
+            using (var webDriver = new ChromeDriver())
+            {
+                webDriver.Url = _baseAddress;
+                var body = webDriver.FindElementByTagName("body").Text;
+                Assert.That(body, Is.Not.Empty);
+            }
+        }
     }
-  }
 }
