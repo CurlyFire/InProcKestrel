377934d313c51106f26a8132a0846e8db587da86
CurlyFire <stephane.denomme@inspq.qc.ca>
2018-03-09 10:14:10 -0500

    Added OldProjectFormat with same fix as NewProjectFormat, however problem still persists

---
+++ InProcKestrel.WebTests.OldProjectFormat/MyTestFixture.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace InProcKestrel.WebTests.OldProjectFormat
+{
+    [TestFixture]
+    public class MyTestFixture
+    {
+        private IWebHost _host;
+        private string _baseAddress;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(@"C:\Git\InProcKestrel\InProcKestrel\")
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+            _host.Start();
+            _baseAddress = _host.ServerFeatures.Get<IServerAddressesFeature>().Addresses.Single();
+        }
+
+        [Test]
+        public void MyTestOldProjectFormat()
+        {
+            using (var webDriver = new ChromeDriver())
+            {
+                webDriver.Url = _baseAddress;
+                var body = webDriver.FindElementByTagName("body").Text;
+                Assert.That(body, Is.Not.Empty);
+            }
+        }
+    }
+}
---
+++ InProcKestrel.WebTests.OldProjectFormat/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InProcKestrel.WebTests.OldProjectFormat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("INSPQ")]
+[assembly: AssemblyProduct("InProcKestrel.WebTests.OldProjectFormat")]
+[assembly: AssemblyCopyright("Copyright © INSPQ 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6fcec49a-19c9-429c-b71f-805d50a0db71")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
